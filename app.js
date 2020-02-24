3eb50fcad3dcf6499c378c76a81ed18424952dcc
Hazri <hazrijordan@gmail.com>
2020-02-24 13:01:41 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+console.log(months.slice(4, 7).join(' '));
+
+// for (let i = 0; i < months.length; i++) {
+//   console.log(months[4], months[5], months[6]);
+//   break;
+// }
+
+// months.forEach(month => {
+//   console.log(month);
+// });
